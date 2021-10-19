d52703e04014611a898bb319b197eb6035d7284c
Ksenx73 <hello@itcampe.com>
2021-10-19 11:50:58 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,10 @@
+package it.campe;
+
+public class Main {
+    private static final String TRIANGLE_EXISTS = "треугольник существует";
+    private static final String TRIANGLE_NOT_EXISTS = "треугольник не существует";
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}

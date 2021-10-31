1c3f667d8413dc496b581e0d623086f1db79de27
JanaZhakPl <janazhakpl@gmail.com>
2021-10-31 11:02:40 +0100

    done

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -1,10 +1,21 @@
 package it.campe;
 
+import java.util.Scanner;
+
 public class Main {
     private static final String TRIANGLE_EXISTS = "треугольник существует";
     private static final String TRIANGLE_NOT_EXISTS = "треугольник не существует";
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+       Scanner s = new Scanner(System.in);
+       int a = s.nextInt();
+       int b = s.nextInt();
+       int c = s.nextInt();
+       if ((a + b) > c && (b + c) > c && (a + c) > b) {
+           System.out.println(TRIANGLE_EXISTS);
+       } else {
+           System.out.println(TRIANGLE_NOT_EXISTS);
+       }
+
     }
 }
